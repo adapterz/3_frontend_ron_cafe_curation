01108651a3286f745f4973c28c9d3e8fb8504fef
devPark <ronny_park@naver.com>
2022-03-09 00:14:34 +0900

    로그인, 회원가입 route 추가

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -5,5 +5,11 @@ const indexRouter = express.Router();
 indexRouter.get('/main', (req, res) => {
   return res.sendFile(path.resolve('public/html/main.html'));
 });
+indexRouter.get('/login', (req, res) => {
+  return res.sendFile(path.resolve('public/html/login.html'));
+});
+indexRouter.get('/register', (req, res) => {
+  return res.sendFile(path.resolve('public/html/register.html'));
+});
 
 module.exports = indexRouter;
