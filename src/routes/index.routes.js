49e17ee4d1561f89a33232a729245268969301eb
devPark <ronny_park@naver.com>
2022-03-09 23:23:02 +0900

    아이디 찾기, 비밀번호 찾기 라우트 추가

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -11,5 +11,11 @@ indexRouter.get('/login', (req, res) => {
 indexRouter.get('/register', (req, res) => {
   return res.sendFile(path.resolve('public/html/register.html'));
 });
+indexRouter.get('/find-email', (req, res) => {
+  return res.sendFile(path.resolve('public/html/findEmail.html'));
+});
+indexRouter.get('/find-password', (req, res) => {
+  return res.sendFile(path.resolve('public/html/findPassword.html'));
+});
 
 module.exports = indexRouter;
