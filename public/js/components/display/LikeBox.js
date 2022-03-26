cdf4f72c19b6c66c1cb122ca0d1e654f1f3244c2
ByungchanPark <pbc9236@gmail.com>
2022-03-26 11:42:14 +0900

    todo: 각 파일 별 투두 및 주의사항 주석 작성

--- public/js/components/display/LikeBox.js
+++ public/js/components/display/LikeBox.js
@@ -1,3 +1,4 @@
+// XXX 5번 이상 재사용하지 않는 경우라면 굳이 컴포넌트화 할 필요가 없음.
 export default function createDisplayLikeBox() {
   const likeBox = document.createElement('div');
   const iconElem = document.createElement('i');
